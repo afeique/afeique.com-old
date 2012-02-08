8f0e5d221c1d385069f3eda94e542f782e540970
afeique <afeique@gmail.com>
2012-02-08 11:25:21 -0500

    added contact block, apparently wasn't included

---
+++ blocks/content/contact.php
@@ -0,0 +1,11 @@
+<ul>
+  <li><strong>email and gtalk</strong> afeique at gmail dot com</li>
+  <li><strong>aim</strong> afeique</li>
+  <li><strong>msn</strong> afeique at gmail dot com</li>
+  <li><strong>skype</strong> afeique</li>
+</ul>
+
+<p>
+  Feel free to contact me with any questions, thoughts, or comments you have. I enjoy listening to other
+  people's ideas and becoming acquainted, so don't hesitate!
+</p>
