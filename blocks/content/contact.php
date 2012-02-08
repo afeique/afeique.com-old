e6020ed34429d0593483402eab1423b57b4daa2c
afeique <afeique@gmail.com>
2012-02-08 11:28:36 -0500

    edited contact page to fix issue with li bullet

--- blocks/content/contact.php
+++ blocks/content/contact.php
@@ -1,9 +1,12 @@
-<ul>
-  <li><strong>email and gtalk</strong> afeique at gmail dot com</li>
-  <li><strong>aim</strong> afeique</li>
-  <li><strong>msn</strong> afeique at gmail dot com</li>
-  <li><strong>skype</strong> afeique</li>
-</ul>
+
+<div class="span-24">
+  <ul>
+    <li><strong>email and gtalk</strong> afeique at gmail dot com</li>
+    <li><strong>aim</strong> afeique</li>
+    <li><strong>msn</strong> afeique at gmail dot com</li>
+    <li><strong>skype</strong> afeique</li>
+  </ul>
+</div>
 
 <p>
   Feel free to contact me with any questions, thoughts, or comments you have. I enjoy listening to other
