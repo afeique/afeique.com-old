08a09df83e81365a065385bbfa3832669b7e2ae5
afeique <afeique@gmail.com>
2012-02-19 19:24:30 -0500

    modified about... no longer in 3rd person, I just couldn't handle the weirdness anymore

--- blocks/content/about.php
+++ blocks/content/about.php
@@ -1,22 +1,22 @@
 <h2>contact</h2>
 <p>
-  To contact Afeique with thoughts, questions, or professional inquiries, feel free to send an e-mail to <?=strong('root at afeique dot com')?>.</p>
+  To contact me with thoughts, questions, or professional inquiries, feel free to send an e-mail to <?=strong('root at afeique dot com')?>.</p>
 
 <h2>about this site</h2>
 <p>
-  This site is meant to serve as an area for Afeique to post things he's learned and created. It is not aimed to disperse his opinions, but to publish certain creations and help him reflect upon his activities.</p>
+  This site is meant to serve as an area for me to post things he's learned and created. It is not aimed to disperse opinions, but to publish certain creations and help me reflect upon my activities.</p>
 
 <h2>about afeique</h2>
 <p>
-  Afeique Sheikh is currently an IT Programmer/Analyst at the <?=b_link('http://rochester.edu/','University of Rochester')?> and a student of Electrical and Computer Engineering (ECE) at <?=b_link('http://cmu.edu/','Carnegie Mellon University')?>.
+  I'm currently an IT Programmer/Analyst at the <?=b_link('http://rochester.edu/','University of Rochester')?> and a student of Electrical and Computer Engineering (ECE) at <?=b_link('http://cmu.edu/','Carnegie Mellon University')?>.
 
 <h2>professional experience</h2>
 <p>
-   Afeique's professional development experience includes deployment and management of best-practice HTML/CSS/JavaScript websites using <?=b_link('http://apache.org/','Apache')?>/<?=b_link('http://dev.mysql.com/','MySQL')?>/<?=b_link('http://php.net/','PHP')?>/<?=b_link('http://jquery.com','jQuery')?> running on LAMP stacks. In his current position, he primarily works with the <?=b_link('http://codeigniter.com/','CodeIgniter')?> MVC framework, and for this site, a personal framework built upon MVC principles without being "strictly MVC." His Resumé/CV is available on request.</p>
+   My professional development experience includes deployment and management of best-practice HTML/CSS/JavaScript websites using <?=b_link('http://apache.org/','Apache')?>/<?=b_link('http://dev.mysql.com/','MySQL')?>/<?=b_link('http://php.net/','PHP')?>/<?=b_link('http://jquery.com','jQuery')?> running on LAMP stacks. In my current job, I primarily work with the <?=b_link('http://codeigniter.com/','CodeIgniter')?> MVC framework. For this site, I created my own framework built upon MVC principles without being "strictly MVC." My Resumé/CV is available on request.</p>
 <ul>
   <li><?=b_link('https://plus.google.com/u/0/106624377204443744556','google+ profile')->_('rel','me')?></li>
   <li><?=b_link('http://www.linkedin.com/pub/afeique-sheikh/8/4b7/bb2','linkedin profile')?></li></ul>
 
 <h2>additional experience</h2>
 <p>
-  Afeique also has a range of experience with other programming languages: perl, python, ruby, c, c++, assembly, shell scripting, and tcl with the tk toolkit. His preferred programming platform is Linux with either emacs or vim (he is partial to both). However, he has just as much professional experience working on Windows and Mac OS X (usually using Eclipse) and is unbiased about the operating system so long as he can "get the job done."</p>
+  I also has a range of experience with other programming languages: perl, python, ruby, c, c++, assembly, shell scripting, and tcl with the tk toolkit. My preferred programming platform is Linux with either emacs or vim (I'm partial to both), but I have just as much professional experience working on Windows and Mac OS X (usually using Eclipse). Consequently, I'm not concerned about what operating system I use as long as I can get the job done.</p>
